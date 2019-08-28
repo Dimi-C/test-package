63ee050dee4af99aae602642d8ed144caa2f400b
Dimitri Collas <dimitricollas@gmail.com>
2019-08-28 14:48:47 -0700

    First Commit

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+(define collection "test-package")
+(define deps '("base"))
+(define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
+(define scribblings '(("scribblings/test-package.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(dimitricollas))
---
+++ scribblings/test-package.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[test-package
+                    racket/base]]
+
+@title{test-package}
+@author{dimitricollas}
+
+@defmodule[test-package]
+
+Package Description Here
